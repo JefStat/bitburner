3af8c69a7bd52e947ad06bb66a3f4e2c1e2910d3
Jef Statham <JefStat@users.noreply.github.com>
2022-01-28 13:36:02 -0500

    Update v3_mcp.js

--- v3_mcp.js
+++ v3_mcp.js
@@ -4,16 +4,20 @@ let ns;
 async function main(pns) {
   ns = pns;
   target = 'n00dles'
+  let lastUpdate = Date.now();
   while (true) {
     let now = Date.now();
-    validate(target, now);
+    let diff = now - lastupdate;
+    lastUpdate = now;
+    validate(target, now, diff);
     const state = getStateForTargetAtTime(target, now);
     await runState(target, state, now);
     await ns.sleep(0);
   }
 }
 
-function validate(target, time) {
+function validate(target, time, diff) {
+	ns.print(`time elaspsed: ${ns.tformat(diff)}`);
 	const hist = targetHistories[target];
 	const server = ns.getServer(target);
 	const states = hist.states.filter(h => h.time < Date.now());
