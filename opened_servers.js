90a83eef114bec99268cdde62727a91c03558374
Jef _ <jef.statham@gmail.com>
2022-01-28 14:30:12 -0500

    update utils

--- opened_servers.js
+++ opened_servers.js
@@ -1,21 +1,19 @@
-import { ensureRootAccess } from 'utils.js';
-async function scan(ns, parent, server, list) {
-    const children = await ns.scan(server);
+function scan(ns, parent, server, list) {
+    const children = ns.scan(server);
     for (let child of children) {
         if (parent == child) {
             continue;
         }
-        await ensureRootAccess(ns, child);
+        ensureRootAccess(ns, child);
         list.push(child);
 
-        await scan(ns, server, child, list);
-        await ns.sleep(10);
+        scan(ns, server, child, list);
     }
 }
 
-export async function list_servers(ns) {
+export function list_servers(ns) {
     const list = [];
-    await scan(ns, '', 'home', list);
+    scan(ns, '', 'home', list);
 
     return list;
 }
@@ -31,10 +29,64 @@ export async function main(ns) {
         return;
     }
 
-    const servers = (await list_servers(ns)).filter(s => ns.hasRootAccess(s)).concat(['home']);
+    const servers = list_servers(ns).filter(s => ns.hasRootAccess(s)).concat(['home']);
     for (const server of servers) {
-        const used = await ns.getServerUsedRam(server);
-        const max = await ns.getServerMaxRam(server) || 1;
+        const used = ns.getServerUsedRam(server);
+        const max = ns.getServerMaxRam(server) || 1;
         ns.tprint(`${server} is opened. ${used} GB / ${max} GB (${(100 * used / max).toFixed(2)}%)`)
     }
+}
+
+export function ensureRootAccess(ns, host) {
+    ns.disableLog('disableLog');
+    ns.disableLog('hasRootAccess');
+    ns.disableLog('getServerNumPortsRequired');
+    ns.disableLog('fileExists');
+
+    if (host === "home" || ns.hasRootAccess(host)) return true;
+    let portsReq = ns.getServerNumPortsRequired(host);
+    let maxPorts = countPorts(ns);
+    // ns.tprint("need to root " + host + " with " + portsReq + " ports required " + maxPorts + " can be opened");
+    if (portsReq <= maxPorts) {
+        if (ns.fileExists("BruteSSH.exe", "home")) {
+            ns.brutessh(host);
+        }
+        if (ns.fileExists("HTTPWorm.exe", "home")) {
+            ns.httpworm(host);
+        }
+        if (ns.fileExists("FTPCrack.exe", "home")) {
+            ns.ftpcrack(host);
+        }
+        if (ns.fileExists("relaySMTP.exe", "home")) {
+            ns.relaysmtp(host);
+        }
+        if (ns.fileExists("sqlInject.exe", "home")) {
+            ns.sqlinject(host);
+        }
+        ns.nuke(host);
+        ns.tprint("nuked " + host);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function countPorts(ns) {
+    let ports = 0
+    if (ns.fileExists("BruteSSH.exe", "home")) {
+        ports++;
+    }
+    if (ns.fileExists("HTTPWorm.exe", "home")) {
+        ports++;
+    }
+    if (ns.fileExists("FTPCrack.exe", "home")) {
+        ports++;
+    }
+    if (ns.fileExists("relaySMTP.exe", "home")) {
+        ports++;
+    }
+    if (ns.fileExists("sqlInject.exe", "home")) {
+        ports++;
+    }
+    return ports
 }
--- utils.js
+++ utils.js
@@ -1,63 +1,37 @@
+import { list_servers } from 'opened_servers.js';
+
 /** @param {NS} ns **/
-export async function ensureRootAccess(ns, host) {
-	ns.disableLog('disableLog');
-	ns.disableLog('hasRootAccess');
-	ns.disableLog('getServerNumPortsRequired');
-	ns.disableLog('fileExists');
-
-	if (host === "home" || await ns.hasRootAccess(host)) return true;
-	let portsReq = await ns.getServerNumPortsRequired(host);
-	let maxPorts = await countPorts(ns);
-	// ns.tprint("need to root " + host + " with " + portsReq + " ports required " + maxPorts + " can be opened");
-	if (portsReq <= maxPorts) {
-		if (await ns.fileExists("BruteSSH.exe", "home")) {
-			await ns.brutessh(host);
-		}
-		if (await ns.fileExists("HTTPWorm.exe", "home")) {
-			await ns.httpworm(host);
-		}
-		if (await ns.fileExists("FTPCrack.exe", "home")) {
-			await ns.ftpcrack(host);
-		}
-		if (await ns.fileExists("relaySMTP.exe", "home")) {
-			await ns.relaysmtp(host);
-		}
-		if (await ns.fileExists("sqlInject.exe", "home")) {
-			await ns.sqlinject(host);
-		}
-		await ns.nuke(host);
-		ns.tprint("nuked " + host);
-		return true;
-	} else {
-		return false;
-	}
-}
-
-async function countPorts(ns) {
-	let ports = 0
-	if (await ns.fileExists("BruteSSH.exe", "home")) {
-		ports++;
-	}
-	if (await ns.fileExists("HTTPWorm.exe", "home")) {
-		ports++;
-	}
-	if (await ns.fileExists("FTPCrack.exe", "home")) {
-		ports++;
-	}
-	if (await ns.fileExists("relaySMTP.exe", "home")) {
-		ports++;
-	}
-	if (await ns.fileExists("sqlInject.exe", "home")) {
-		ports++;
-	}
-	return ports
-}
-
-export const getCores = (host) => ns.getServer(host).cpuCores;
-
-export async function runHackScript(script, host, threads, target) {
+export async function runHackScript(ns, script, host, threads, target) {
 	if (!ns.fileExists(script, host)) {
 		await ns.scp(script, host);
 	}
-	return await ns.exec(script, host, threads, target, Math.random());
+	return Promise.resolve(ns.exec(script, host, threads, target, Math.random()));
+}
+
+/** @param {NS} ns **/
+export function getHosts(ns, scriptRam) {
+	const openedServers = list_servers(ns).filter(s => ns.hasRootAccess(s) && ns.getServerMaxRam(s) > 1);
+	const serversForExecution = ns.getPurchasedServers().concat(openedServers);
+	const s = ns.getServer('home');
+	// reserve some ram for other scripts
+	if (s.maxRam >= 64) {
+		s.maxRam = s.maxRam - 32;
+	} else if (host === 'home' && s.maxRam === 32) {
+		s.maxRam = s.maxRam - 16;
+	}
+	const hostServers = [{
+		server: s,
+		cpuCores: s.cpuCores,
+		threadsAvailable: Math.max(Math.floor((s.maxRam - s.ramUsed) / scriptRam), 0)
+	}];
+	for (const host of serversForExecution) {
+		const s = ns.getServer(host);
+		const threadsAvailable = Math.max(Math.floor((s.maxRam - s.ramUsed) / scriptRam), 0);
+		hostServers.push({
+			server: s,
+			cpuCores: s.cpuCores,
+			threadsAvailable
+		});
+	}
+	return hostServers;
 }
