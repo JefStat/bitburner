4dc22ee9ca085ec4a71ff0d7bc10a16170f6f7be
Jef _ <jef.statham@gmail.com>
2022-02-09 20:49:12 -0500

    all expressions update

--- autosolver.js
+++ autosolver.js
@@ -357,20 +357,20 @@ function arrayJump(arr) {
 
 
 // works for simple answers locks up a browser for large ones
-function allExpressions(data) {
-    const digits = data[0].split('')
+function allExpressions( data ) {
+    const digits = data[0].split( '' )
     const operators = ['+', '-', '*', '']
     let expressions = [digits[0], '-' + digits[0]]
-        .flatMap(d => operators.map(op => d + op))
-    for (let i = 1; i < digits.length - 1; i++) {
+        .flatMap( d => operators.map( op => d + op ))
+    for( let i = 1; i < digits.length - 1; i++ ) {
         expressions = expressions
-            .flatMap(e => operators.map(op => e + digits[i] + op))
+            .flatMap( e => operators.map( op => e + digits[i] + op) )
     }
-    return expressions.map(e => e + digits[digits.length - 1])
-        .filter(e => {
-            try { return eval(e) === data[1] }
-            catch (e) { return false }
-        })
+    return expressions.map( e => e + digits[digits.length - 1] )
+        .filter( e => {
+            try{ return eval(e) === data[1] }
+            catch(e) { return false }
+        } )
 }
 
 // Sanitize Parentheses in Expression
